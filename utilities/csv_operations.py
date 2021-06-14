ff980b589b69f79e39b28288d310d9b49ff229e4
Omar Terrazas <terrazas.omar@gmail.com>
2021-06-14 18:50:16 -0500

    Se agrega función para leer calificaciones de estudiantes.

--- utilities/csv_operations.py
+++ utilities/csv_operations.py
@@ -2,7 +2,7 @@ import csv
 from setup.config import report_file
 from setup.config import grade_file
 
-def populate():
+def read_students():
     students = []
 
     with open(report_file) as csv_file:
@@ -11,4 +11,18 @@ def populate():
         for row in csv_reader:
             students.append((row[0],row[3],row[2],row[4],row[5],row[6],row[7],row[8].lower(),row[9],row[10],row[11],row[12],row[13]))
             line_count += 1
+        print("Found {} students".format(line_count))
     return students
+
+
+def read_grades():
+    grades = []
+
+    with open(grade_file) as csv_file:
+        csv_reader = csv.reader(csv_file, delimiter=',')
+        line_count = 0
+        for row in csv_reader:
+            grades.append((row[2],row[3]))
+            line_count += 1
+        print("Found {} student grades".format(line_count))
+    return grades
