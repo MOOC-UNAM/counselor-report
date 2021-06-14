291a4c3e2eeba2c0cd4d8f3f22f0059a94f00572
Omar Terrazas <terrazas.omar@gmail.com>
2021-06-14 15:30:13 -0500

    Archivo de configuración.

---
+++ setup/config.py
@@ -0,0 +1,18 @@
+## Database config
+DB_NAME = "counselor"
+db_user = "counselor"
+db_pass = "secret!"
+
+
+## Apache server location
+html_dir = "/var/www/html/counselor-report/"
+url_location = "http://localhost/counselor-report/"
+
+
+## Location for PDF created files
+pdf_dir = "pdf/"
+
+
+## Location for CSV files
+report_file = "csv/report.csv"
+grade_file = "csv/grade.csv"
